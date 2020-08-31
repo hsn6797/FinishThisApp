b731acdae01a44d5f4bfc93c1b56f0e6433e59aa
hsn6797 <hsn6797@gmail.com>
2020-08-31 17:00:21 +0500

    Edit screen has been done

--- FinishThisApp/Controllers/EditProfileViewController.swift
+++ FinishThisApp/Controllers/EditProfileViewController.swift
@@ -8,13 +8,14 @@
 
 import UIKit
 import  FirebaseAuth
+import FBSDKLoginKit
+import FirebaseFirestore
 
 class EditProfileViewController: UIViewController {
     
-    
-    
     var CurrentUID = ""
-    
+    var CurrentEmail = ""
+
     @IBOutlet weak var userName: UITextField!
     @IBOutlet weak var EmailTxt: UITextField!
     
@@ -27,9 +28,24 @@ class EditProfileViewController: UIViewController {
         self.navigationItem.leftBarButtonItem = back
         self.navigationItem.title = "Edit profile"
         
-        if Auth.auth().currentUser != nil{
-            CurrentUID = (Auth.auth().currentUser?.uid)!
+        if FBSDKAccessToken.current() != nil {
+            Alert(Message: "", title: "Logined from Facebook")
+        }else{
+            if Auth.auth().currentUser != nil{
+                CurrentUID = (Auth.auth().currentUser?.uid)!
+                CurrentEmail = (Auth.auth().currentUser?.email)!
+                EmailTxt.text = CurrentEmail
+                
+                Firestore.firestore().getCurrentUser(userID: CurrentUID) { user in
+                    
+                    self.userName.text = user.userName
+                }
+                
+                
+                //            userName.text =
+            }
         }
+        
 
         // Do any additional setup after loading the view.
     }
@@ -39,9 +55,46 @@ class EditProfileViewController: UIViewController {
         self.navigationController?.popViewController(animated: true)
     }
     
+    func Alert (Message: String,title:String){
+        
+        let alert = UIAlertController(title: title, message: Message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: alerbtn))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    @objc func alerbtn(_:UIAlertAction){
+        self.navigationController?.popViewController(animated: true)
+    }
+    
 
     @IBAction func SaveButton(_ sender: Any) {
         
+        guard let currentUser = Auth.auth().currentUser else{return}
+        
+        if EmailTxt.text == nil || userName.text == nil || PasswordTxt.text == nil{
+            return
+        }
+        currentUser.updateEmail(to: EmailTxt.text!){ error in
+            if let error = error {
+                error.errorMesssage()
+                return
+            } else {
+                // Email updated.
+                currentUser.updatePassword(to: self.PasswordTxt.text!) { error in
+                    if let error = error {
+                        error.errorMesssage()
+                        return
+                    } else {
+                        // Password updated.
+                        print("success")
+                        let user = User()
+                        user.userID = currentUser.uid
+                        user.userName = self.userName.text!
+                        Firestore.firestore().add(user: user)
+                    }
+                }
+            }
+        }
         
     }
 }
